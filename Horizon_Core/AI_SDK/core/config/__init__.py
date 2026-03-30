f7a6a35971af9e64ef63af2085492960bf6bf4c1
AiHorizons <w2794995038@gmail.com>
2026-03-30 13:17:15 +0800

    Delete Horizon_Core directory

--- Horizon_Core/AI_SDK/core/config/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-配置模块 - 提供SDK的配置管理
-"""
-
-from .config import Config
- 
-__all__ = ['Config'] 
--- Horizon_Core/AI_SDK/providers/deepseek/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-DeepSeek适配器模块
-"""
-
-from .llm_provider import DeepSeekLLMProvider
- 
-__all__ = ['DeepSeekLLMProvider'] 
